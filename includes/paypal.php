2b9eeb8c0df166b527e1eefe981e51abe34efdd3
Phool Dx <59568224+Phool-Frontend@users.noreply.github.com>
2020-05-02 14:57:58 -0500

    goto metiendo de

--- includes/paypal.php
+++ includes/paypal.php
@@ -4,12 +4,12 @@ require 'paypal/autoload.php';
 // Este es donde nos rediriguira cuando Paguemos en Paypay, esta en localhost 
 //define('URL_SITIO','http://localhost/proyecto-produccion');
 
-define('URL_SITIO','https://gdwebcamp.herokuapp.com');
+define('URL_SITIO','https://www.gdwebcamp.herokuapp.com');
 
 $apiContext = new \PayPal\Rest\ApiContext(
     new \PayPal\Auth\OAuthTokenCredential(
-        'ARi3VfElwWBi86Em-_QhmiLZh90lRkXPzAJckhNjeab0EfUhdf7Uvh4bjJ2AnvKkuumxIfsKR5Zwayp_',//Cliente
-        'EOWGJBUMbRRTmgWiht4JQdyPSzDfCn_JbUUvAmo0HmF3KFK6W1Jx8FtgkGpj4ZAKOcQJQvcdDpp1ecCq'//Secret
+        'ATeYyX9QootgJThVhSETUGp-0zGVm9ZLuk9U6vqW8IYapjPInfSjg9tU2ZBKm-MuBMuu9QbDJ8C3VRTb',//Cliente
+        'EJaPsDHJrHiiqacuYAauhtbUIQrYR2pY5ZDvKTK1EyRxmLlWpwRiKOCdXBokjH1keHuA5pMPwKRH-7dA'//Secret
     )
 );
 
